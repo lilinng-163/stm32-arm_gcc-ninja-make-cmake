cb8eb873e183f3cbeb72aef1d9eca744a7995b6f
lilinng-163 <wangyixiang051129@163.com>
2026-04-19 22:52:50 +0800

    arm_none_eabi_gcc_cmake_ninja/make构建stm32工程模板

---
+++ fire.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OPENOCD_PATH="/d/openocd/xpack-openocd-0.12.0-7-win32-x64/xpack-openocd-0.12.0-7/openocd"
+
+
+openocd -f ${OPENOCD_PATH}/scripts/interface/stlink-v2-1.cfg \
+        -f ${OPENOCD_PATH}/scripts/target/stm32f1x.cfg \
+        -c "init" \
+        -c "halt" \
+        -c "flash write_image erase build/template.bin 0x08000000" \
+        -c "verify_image build/template.bin 0x08000000" \
+        -c "reset run" \
+        -c "shutdown" 
